fc1671211312a5a36a61780625f4a1e7b862c196
smhoody <smhoody@gmail.com>
2021-01-05 13:29:09 -0500

    Updated Baker Bot and Leaderboard
    Added !gwins command to baker bot program. Restructured the leaderboard file so that it can be used to create a leaderboard for things other than just bread.

--- Leaderboard.py
+++ Leaderboard.py
@@ -1,15 +1,16 @@
 import os.path
 import matplotlib.pyplot as plt
+import time
+from os import path
 
-
-def leaderboard():
-    completeName = os.path.join("User levels/", ",List of Account names.txt")
+def leaderboard(folder, listFile, userFileExt, leaderboardType):
+    completeName = os.path.join(folder, listFile)
     file = open(completeName, "r")
     userList = []
     userList = file.readline().split(',') #list of each user's name that has bread
     file.close()
-    
-    pointsList = getUserPointsList(userList) #list of each user's points
+
+    pointsList = getUserPointsList(userList, userFileExt) #list of each user's points
 
     temp = 0
     tempName = ""
@@ -35,41 +36,59 @@ def leaderboard():
         i += 1
 
 
-
     plt.style.use("ggplot")
-
-    #create a list of the bread value in exponent form (e.g.: 10^x)
+    
     breads = []
-    for j in range(0, len(pointsList)):
-        breads.append(len(str(pointsList[j])) - 1)
-
-    #create a list of usernames without the tag (e.g.: subscript #1111 away)
+    xLabel = ""
+    if (leaderboardType == "bread"):
+        for j in range(0, len(pointsList)):
+            breads.append(len(str(pointsList[j])) - 1)
+        xLabel = "Amount of bread (10^x)"
+        
+    elif (leaderboardType == "guessing game"):
+        for j in range(0, len(pointsList)):
+            breads.append(pointsList[j])
+        xLabel = "Wins in Guessing Game"   
     names = []
     for k in range(0, len(userList)):
         names.append(userList[k][:-5])
 
     x_pos = [i for i, _ in enumerate(names)]
     
-    plt.barh(x_pos, breads, color = 'blue') #create a horizontal bar graph
-    plt.ylabel("Users") #y label is users
-    plt.xlabel("Amount of bread (10^x)") #x label is bread
-    plt.title("LEADERBOARD") 
-    plt.yticks(x_pos, names) #place usernames
-    plt.savefig('leaderboard.png', bbox_inches = 'tight', pad_inches = 0.1) #save graph
-
+    plt.barh(x_pos, breads, color = 'blue')
+    plt.ylabel("Users")
+    plt.xlabel(xLabel)
+    plt.title("LEADERBOARD")
+    plt.yticks(x_pos, names)
+    plt.savefig('Other files/leaderboard.png', bbox_inches = 'tight', pad_inches = 0.1)
     
-    return 'leaderboard.png'
+    return 'Other files/leaderboard.png'
 
 
 
 
-def getUserPointsList(userList):
+def getUserPointsList(userList, userFileExt):
     userPointsList = []
     
     for count in range(0, len(userList)):
-        userPath = os.path.join("User levels/", userList[count] + ".txt")
+        userPath = os.path.join("User levels/", userList[count] + "" + userFileExt)
+        checkIfFileExists(userPath)
         userFile = open(userPath, "r")
         userPoints = int(userFile.readline())
         userPointsList.append(userPoints)
+        
 
     return userPointsList
+
+
+def checkIfFileExists(pathName):
+    exists = False
+    if (not path.exists(pathName)):
+        fileW = open(pathName, "w")
+        fileW.write("0")
+        fileW.close()
+        
+    else:
+        exists = True
+
+    return exists
